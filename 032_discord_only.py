2bb2ef81f04b6ebf3763fe493b9d5ce6c11a3ef0
Liam Schroth <liam.schroth@gmail.com>
2020-05-16 13:49:29 -0700

    check for existing record by ID number

--- 032_discord_only.py
+++ 032_discord_only.py
@@ -144,7 +144,7 @@ async def lookForRecord():
             print("checking records")
             newRecords = await apiRecentWRs()
             for record in newRecords:
-                if record not in oldRecords:
+                if record['id'] not in list(map(lambda x: x.id, oldRecords)):
                     print("announcing!")
                     await announce(record)
         except:
