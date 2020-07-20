5834a1d573a0480305c396a77d38a4a7ead737af
wackeeeEEE <wackeesmite@gmail.com>
2020-07-19 19:11:41 -0700

    broken float test

--- 032_discord_only.py
+++ 032_discord_only.py
@@ -10,6 +10,8 @@ import discord
 from discord.ext.commands import Bot
 import csv
 import sys
+import datetime
+import math
 print(discord.__version__)
 mb = Bot(command_prefix='!')
 TOKEN = open("TOKEN.txt", "r").readline()
@@ -19,7 +21,47 @@ NOTIFS_CHANNEL_ID = 491719347929219072
 RECORDS_CHANNEL_ID =  600075722232692746
 NO_STREAMS_TEXT = "Nobody is currently streaming" + "<:NotLikeThis:257718094049443850>"
 SOME_STREAMS_TEXT = "CURRENTLY LIVE:\n- - - - - - - - - - - - -"
+RELAY = False
 
+@mb.event
+async def on_message(message):
+    if message.content.lower() == ".sc_fling":
+        await message.channel.send(
+            " It is known. It skips the plasmas from Goldie so its not really worth. It would save time in coop or if you picked up plasmas before the fling."
+            )
+#    if message.content.lower() == ".relay":
+#        delta = await relayCountdown(ret=True)
+#        await message.channel.send(embed=discord.Embed(description=delta))
+    if ".scarab_deload" == message.content.lower().split()[0]:
+        await message.channel.send(
+                "If you reach the top of the stairs between 1:14 and 1:20 from the Scarab's initial spawn, it will deload. Full explanation at https://www.youtube.com/watch?v=j9fgKI74dwo"
+                )
+    if ".points" == message.content.lower().split()[0]:
+        if len(message.content.lower().split()) == 3:
+            cmd = message.content.lower().split()
+            points = await getPoints(cmd[1], cmd[2])
+            await message.channel.send(points)
+    if ".scoped" == message.content.lower().split()[0]:
+        await message.channel.send("Scoped flies are different than unscoped; read: https://discordapp.com/channels/83061848139632640/293842240529235968/727967537114906657")
+    if ".calc" in message.content.lower():
+        if len(message.content.lower().split()) == 2:
+            game = message.content.lower().split()[1]
+            if game == "reach":
+                await message.channel.send("https://haloruns.com/timeCalc/reach")                
+            if game in ["h1", "hce", "ce"]:
+                await message.channel.send("https://haloruns.com/timeCalc/hce") 
+            if game == "h2":
+                await message.channel.send("https://haloruns.com/timeCalc/h2") 
+            if game == "h2a":
+                await message.channel.send("https://haloruns.com/timeCalc/h2a") 
+            if game == "h3":
+                await message.channel.send("https://haloruns.com/timeCalc/h3") 
+            if game == "odst":
+                await message.channel.send("https://haloruns.com/timeCalc/odst") 
+            if game == "h4":
+                await message.channel.send("https://haloruns.com/timeCalc/h4") 
+            if game == "h5":
+                await message.channel.send("https://haloruns.com/timeCalc/h5") 
 
 async def getProfile(user):
     return requests.get(str(Endpoint + "users/" + user)).json()
@@ -65,7 +107,7 @@ async def announce(record):
                                   
 async def maintainTwitchNotifs():
     while True:
-        await asyncio.sleep(1220)
+        await asyncio.sleep(120)
         print("looking for streams to post")
         responses = []
         postedStreamList = []
@@ -73,14 +115,7 @@ async def maintainTwitchNotifs():
         try:
             streams = requests.get(str(Endpoint + "streams")).json()
         except:
-            print("failed to pull from main API, proceeding to backup...")
-        try:
-            if streams:
-                pass
-        except:
-            streams = requests.get(str(BETA_ENDPOINT + "streams")).json()# = requests.get(str(Endpoint + "streams" + ResponseFormat)).json()
-##        except:
-##            print("stream pull failed")
+            print("stream pull failed")
         postedStreams = await mb.get_channel(NOTIFS_CHANNEL_ID).history(oldest_first = True).flatten()
         postedStreams = postedStreams[1:]
         for stream in postedStreams:
@@ -105,6 +140,7 @@ async def maintainTwitchNotifs():
 
 async def purgeNotStreams(streams):
     flat = await mb.get_channel(NOTIFS_CHANNEL_ID).history(oldest_first=True).flatten()
+##    await relayCountdown()
     oldestMessage = flat[0]
 ##    streams = {}
     if streams != []:
@@ -113,13 +149,13 @@ async def purgeNotStreams(streams):
         for messageObject in flat:
             if messageObject == oldestMessage:
                 if messageObject.content != SOME_STREAMS_TEXT:   
-                    print("Streams found, editing top message")
-                    await messageObject.edit(content = SOME_STREAMS_TEXT)
+                    if RELAY==False:
+                        print("Streams found, editing top message")
+                        await messageObject.edit(content = SOME_STREAMS_TEXT)
                 else:
                     print("Found streams, continuing...")
-##            config = loadConfig()
 ##            else:
-##                config["#" + messageObject.content.split("/")[-1].lower()]["muted"] == True
+####                config["#" + messageObject.content.split("/")[-1].lower()]["muted"] == True
 ##                await messageObject.delete()
             if messageObject.content not in streams:
                 if messageObject == oldestMessage:
@@ -132,9 +168,42 @@ async def purgeNotStreams(streams):
         messagesLen = len(flat)
         for messageObject in flat:
             if messageObject == oldestMessage:
-                await messageObject.edit(content=NO_STREAMS_TEXT)
+                if RELAY==False:
+                    await messageObject.edit(content=NO_STREAMS_TEXT)
             else:
                 await messageObject.delete()
+
+async def relayCountdown(ret=False):
+    if ret == True:
+        flat = await mb.get_channel(NOTIFS_CHANNEL_ID).history(oldest_first=True).flatten()
+        dt = datetime.datetime
+        now = dt.now()
+        relayStartTime = dt(year=2020, month=6, day=28, hour=6)
+        delta = relayStartTime - now
+        return str(':'.join(str(delta).split(':')[:2])) + " until the HaloRuns Relay Race!\nWatch [Here](https://www.twitch.tv/HaloRaces)"
+    while True:
+        await asyncio.sleep(10)
+        if RELAY==True:
+            flat = await mb.get_channel(NOTIFS_CHANNEL_ID).history(oldest_first=True).flatten()
+            dt = datetime.datetime
+            now = dt.now()
+            relayStartTime = dt(year=2020, month=6, day=28, hour=6)
+            delta = relayStartTime - now
+            oldestMessage = flat[0]            
+            await oldestMessage.edit(content = str(':'.join(str(delta).split(':')[:2])) + " until the HaloRuns Relay Race!")
+
+async def getPoints(pb, wr):
+    pb_split = pb.split(":")
+    pb_mins = int(pb_split[0])
+    pb_secs = int(pb_split[1])
+    pb_comb = pb_secs + 60 * pb_mins
+    wr_split = wr.split(":")
+    wr_mins = int(wr_split[0])
+    wr_secs = int(wr_split[1])
+    wr_comb = wr_secs + 60 * wr_mins
+    points = round((0.008 * math.exp(4.8284*(wr_comb/pb_comb)) * 100, 1))
+    print(str("Your PB of " + pb + " against "  + wr + " is worth " + str(points) + " points"))
+    return(str("Your PB of " + pb + " against "  + wr + " is worth " + str(points) + " points"))
 
 async def lookForRecord():
     while True:
@@ -143,8 +212,11 @@ async def lookForRecord():
             oldRecords = await savedRecentWRs()
             print("checking records")
             newRecords = await apiRecentWRs()
+            ids = []
+            for element in oldRecords:
+                ids.append(element["id"])
             for record in newRecords:
-                if record not in oldRecords:
+                if record['id'] not in ids:
                     print("announcing!")
                     await announce(record)
         except:
@@ -182,10 +254,11 @@ def streams(streams):
         streamList.append(stream["stream"])
     return streams
 
-##def loadConfig():
-##    file = json.load(open("config.json", "r+"))
-##    return file
-##    
+def loadConfig():
+    file = json.load(open("config.json", "r+"))
+    return file
+
+
 def parseStream(stream):
     val = URLValidator()
     newTitleList = []
@@ -212,6 +285,13 @@ def parsePlayers(record):
             players.append(buildPlayerMD(player))
     return " | ".join(players)
 
+#async def maintainRelayCountdown():
+ #   string = "Relay Begins in:"
+  #  flat = await mb.get_channel(NOTIFS_CHANNEL_ID).history(oldest_first=True).flatten()
+
+    
+
+mb.loop.create_task(relayCountdown())
 mb.loop.create_task(lookForRecord())
 mb.loop.create_task(maintainTwitchNotifs())
 mb.run(TOKEN)
